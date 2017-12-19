4c9f8afe6decfdbdc8ab912a37e005fef16460e9
matheus <freeuser153>
2017-12-18 22:44:06 -0200

    Footer working

---
+++ src/common/template/footer.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default props => (
+  <footer className='main-footer'>
+    <strong>
+      Copyright &roBOT corporation; 2017
+      <a href='https://github.com/matheusalxds' target='_black'> Matheus Alexandre </a>
+    </strong>
+  </footer>
+)
