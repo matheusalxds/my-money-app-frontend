9a3e8e9e11db776bc5dc56a1218d142f1bacccec
matheus <freeuser153>
2018-01-04 22:42:56 -0200

    Showing billing cycle list

--- src/billingCycle/billingCycleActions.js
+++ src/billingCycle/billingCycleActions.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 const BASE_URL = 'http://localhost:3003/api';
 
 export function getList() {
-  const request = axios.get(`${BASE_URL}/billingCycles`);
+  const request = axios.get(`${BASE_URL}/billingCycle`);
   return {
     type: 'BILLING_CYCLLE_FETCHED',
     payload: request,
