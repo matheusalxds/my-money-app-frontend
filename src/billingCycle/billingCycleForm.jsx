dea5dddade44322ee2f044a0e91b043d8e80f21d
matheus <freeuser153>
2018-01-05 10:57:28 -0200

    Added button 'cancelar', initializing the fields with null and setting tabs to initial state

--- src/billingCycle/billingCycleForm.jsx
+++ src/billingCycle/billingCycleForm.jsx
@@ -1,4 +1,7 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import { init } from './billingCycleActions';
 import { reduxForm, Field } from 'redux-form';
 import labelAndInput from '../common/form/labelAndInput';
 
@@ -20,10 +23,13 @@ class BillingCycleForm extends Component {
         </div>
         <div className='box-footer'>
           <button type='submit' className='btn btn-primary'>Submit</button>
+          <button type='button' className='btn btn-default' onClick={this.props.init}>Cancelar</button>
         </div>
       </form>
     );
   }
 }
 
-export default reduxForm({ form: 'billingCycleForm' })(BillingCycleForm);
+BillingCycleForm = reduxForm({ form: 'billingCycleForm', destroyOnUnmount: false })(BillingCycleForm);
+const mapDispatchToProps = dispatch => bindActionCreators({ init }, dispatch);
+export default connect(null, mapDispatchToProps)(BillingCycleForm);
--- src/billingCycle/billingCycleList.jsx
+++ src/billingCycle/billingCycleList.jsx
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
-import { getList } from './billingCycleActions';
+import { getList, showUpdate } from './billingCycleActions';
 
 class BillingCycleList extends Component {
 
@@ -17,12 +17,16 @@ class BillingCycleList extends Component {
         <td>{bc.name}</td>
         <td>{bc.month}</td>
         <td>{bc.year}</td>
+        <td>
+          <button className='btn btn-warning' onClick={() => this.props.showUpdate(bc)}>
+            <i className='fa fa-pencil'/>
+          </button>
+        </td>
       </tr>
     ))
   }
 
   render(){
-    console.log('LIST', this.props.list);
     return(
       <div>
         <table className='table'>
@@ -30,7 +34,8 @@ class BillingCycleList extends Component {
             <tr>
               <th>Nome</th>
               <th>Mês</th>
-              <th>Ano</th>              
+              <th>Ano</th>
+              <th>Ações</th>
             </tr>
           </thead>
           <tbody>
@@ -45,5 +50,5 @@ class BillingCycleList extends Component {
 }
 
 const mapStateToProps = state => ({ list: state.billingCycle.list });
-const mapDispatchToProps = dispatch => bindActionCreators({ getList }, dispatch);
+const mapDispatchToProps = dispatch => bindActionCreators({ getList, showUpdate }, dispatch);
 export default connect(mapStateToProps, mapDispatchToProps)(BillingCycleList);
