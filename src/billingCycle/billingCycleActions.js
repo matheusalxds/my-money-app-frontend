d981a100460f4fd107324b38a12f091be96b666b
matheus <freeuser153>
2018-01-05 11:56:54 -0200

    Edit working

--- src/billingCycle/billingCycleActions.js
+++ src/billingCycle/billingCycleActions.js
@@ -17,8 +17,18 @@ export function getList() {
 }
 
 export function create(values){
+  return submit(values, 'post');
+}
+
+export function update(values) {
+  return submit(values, 'put');
+}
+
+function submit(values, method) {
   return dispatch => {
-    axios.post(`${BASE_URL}/billingCycle`, values)
+    const id = values._id ? values._id : '';
+
+    axios[method](`${BASE_URL}/billingCycle/${id}`, values)
       .then(resp => {
         toastr.success('Sucesso', 'Operação realizada com sucesso.');
         dispatch(init());
